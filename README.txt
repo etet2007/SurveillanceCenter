a9d5d93e918a5f934cb7463de64a69e9ebe28caa
Lqtian <120220149@qq.com>
2016-10-09 17:15:34 +0800

    Add some comments and ready to improve the code structure.

--- README.txt
+++ README.txt
@@ -1,4 +1,6 @@
 程序结构：
 main.cpp：程序入口。使用QApplication对象设定字体，显示程序主页面对象MyQMainWidget。
 cuttingNode类：切分节点模型类。
-recombinationNode：重组节点模型类。
+
+recombinationNode.h /.cpp：重组节点模型类。
+cuttingNode.h /.cpp：切分节点模型类。
--- recombinationnode.h
+++ recombinationnode.h
@@ -14,28 +14,27 @@ public:
 
     QList<QString> receivedList()const;
     void setReceivedList(const QList<QString> &receivedList);
+    void appendReceivedList(QString &);
+    QString getReceivedListString();
 
     int id() const;
     void setId(int id);
-
-    void appendReceivedList(QString &);
+    //当前IP
     QString ip() const;
     void setIp(const QString &ip);
-
+    //位置信息字符串
     QString position() const;
     void setPosition(const QString &position);
     void appendPosition(QPointF &point);
 
-    QString getReceivedListString();
-
     void clearReceivedListAndPosition();
+
     QString getFourPointsList() const;
 
 private:
-    int m_id;  //id编号
+    int m_id;  //当前ID编号
     QString m_ip;//重组节点ip地址
-
-    QList<QString> m_receivedList; //存储切分节点ip  顺序不是固定的
+    QList<QString> m_receivedList; //接收列表，每个元素为切分节点IP
 
     QString m_position;//不用
     QString fourPointsList;
